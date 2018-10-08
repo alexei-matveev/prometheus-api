a9f5eec72749072316ede8f725e01c1e12b3e7a2
Alexei Matveev <alexei.matveev@gmail.com>
2018-10-08 14:48:47 +0200

    AM: as json with cheshire on classpath

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.9.1"]]
+                 [clj-http "3.9.1"]
+                 [cheshire "5.8.1"]]
   :main prometheus-api.core)
--- src/prometheus_api/core.clj
+++ src/prometheus_api/core.clj
@@ -7,11 +7,21 @@
         resp (http/get url
                        {:query-params query-params
                         :multi-param-style :array
+                        :as :json
+                        :debug? false})]
+    (:body resp)))
+
+(defn- query [base-url query-params]
+  (let [url (str base-url "/api/v1/query")
+        resp (http/get url
+                       {:query-params query-params
+                        :multi-param-style :array
+                        :as :json
                         :debug? false})]
     (:body resp)))
 
 (defn -main
   [& args]
-  (let [url "http://localhost:9090"
-        body (series url {:match ["up"]})]
-    body))
+  (let [url "http://localhost:9090"]
+    [(series url {:match ["up"]})
+     (query url {:query "up"})]))
