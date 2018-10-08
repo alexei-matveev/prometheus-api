42d7eaf12d57b46e0a4acc4cf52064faa37788b4
Alexei Matveev <alexei.matveev@gmail.com>
2018-10-08 18:11:41 +0200

    AM: make-selector

--- src/prometheus_api/core.clj
+++ src/prometheus_api/core.clj
@@ -1,6 +1,12 @@
+;;;
+;;; Prometheus HTTP API [1]
+;;;
+;;; [1] https://prometheus.io/docs/prometheus/latest/querying/api/
+;;;
 (ns prometheus-api.core
   (:require
    [clj-http.client :as http]
+   [clojure.string :as cs]
    [clojure.pprint :as pp]))
 
 (defn- exec [url query-params]
@@ -9,20 +15,47 @@
               :as :json
               :debug? false}
         resp (http/get url opts)]
-    ;; Check for :status = "error" or just for :error in the body:
+    ;; Check for :status = "error" look for :error in the body. Data
+    ;; may be still present even if the error occured:
     (-> resp :body :data)))
 
+;; GET /api/v1/series
 (defn- series [base-url query-params]
   (let [url (str base-url "/api/v1/series")]
     (exec url query-params)))
 
+;; GET /api/v1/query
 (defn- query [base-url query-params]
   (let [url (str base-url "/api/v1/query")]
     (exec url query-params)))
+
+;; GET /api/v1/query_range
+;; GET /api/v1/label/<label_name>/values
+;; GET /api/v1/targets
+;; GET /api/v1/rules
+;; GET /api/v1/alerts
+;; GET /api/v1/targets/metadata
+;; GET /api/v1/alertmanagers
+;; GET /api/v1/status/config
+;; GET /api/v1/status/flags
+
+(defn- make-selector [obj]
+  (let [stem (:__name__ obj)
+        labels (dissoc obj :__name__)]
+    (str stem
+         "{"
+         (apply str
+                (cs/join ","
+                         (for [[k v] labels]
+                           (str (name k) "=" (pr-str v)))))
+         "}")))
 
 (defn -main [& args]
   (let [url "http://localhost:9090"
         res {:series (series url {:match ["up"]})
              :query (query url {:query "up"})}]
-    res
-    #_(pp/pprint res)))
+    (pp/pprint res)
+    (pp/pprint
+     (let [ms (series url {:match ["go_gc_duration_seconds"]})]
+       (for [m ms]
+         (make-selector m))))))
