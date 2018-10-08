3ef730068bb316022188f81787750193c9c327ef
Alexei Matveev <alexei.matveev@gmail.com>
2018-10-08 17:32:29 +0200

    AM: share more

--- src/prometheus_api/core.clj
+++ src/prometheus_api/core.clj
@@ -1,29 +1,28 @@
 (ns prometheus-api.core
-  (:require [clj-http.client :as http]))
+  (:require
+   [clj-http.client :as http]
+   [clojure.pprint :as pp]))
 
-(defn- exec [url opts]
-  (let [resp (http/get url opts)]
+(defn- exec [url query-params]
+  (let [opts {:query-params query-params
+              :multi-param-style :array
+              :as :json
+              :debug? false}
+        resp (http/get url opts)]
     ;; Check for :status = "error" or just for :error in the body:
     (-> resp :body :data)))
 
 (defn- series [base-url query-params]
-  (let [url (str base-url "/api/v1/series")
-        opts {:query-params query-params
-              :multi-param-style :array
-              :as :json
-              :debug? false}]
-    (exec url opts)))
+  (let [url (str base-url "/api/v1/series")]
+    (exec url query-params)))
 
 (defn- query [base-url query-params]
-  (let [url (str base-url "/api/v1/query")
-        opts {:query-params query-params
-              :multi-param-style :array
-              :as :json
-              :debug? false}]
-    (exec url opts)))
+  (let [url (str base-url "/api/v1/query")]
+    (exec url query-params)))
 
-(defn -main
-  [& args]
-  (let [url "http://localhost:9090"]
-    [(series url {:match ["up"]})
-     (query url {:query "up"})]))
+(defn -main [& args]
+  (let [url "http://localhost:9090"
+        res {:series (series url {:match ["up"]})
+             :query (query url {:query "up"})}]
+    res
+    #_(pp/pprint res)))
