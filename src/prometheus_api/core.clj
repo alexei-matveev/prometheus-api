14494e85377aec10355e1dd37b4d0574a1c0b10f
Alexei Matveev <alexei.matveev@gmail.com>
2018-10-08 14:36:07 +0200

    AM: series

--- src/prometheus_api/core.clj
+++ src/prometheus_api/core.clj
@@ -1,10 +1,17 @@
 (ns prometheus-api.core
   (:require [clj-http.client :as http]))
 
-(defn -main
-  [& args]
-  (let [resp (http/get "http://localhost:9090/api/v1/series"
-                       {:query-params {:match ["up"]}
+;; Error checking?
+(defn- series [base-url query-params]
+  (let [url (str base-url "/api/v1/series")
+        resp (http/get url
+                       {:query-params query-params
                         :multi-param-style :array
                         :debug? false})]
-    (println (:body resp))))
+    (:body resp)))
+
+(defn -main
+  [& args]
+  (let [url "http://localhost:9090"
+        body (series url {:match ["up"]})]
+    body))
